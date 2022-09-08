a0c39efb6af4b2dc5b3458b811dcc2088453511b
Anas <a.elkouaiti@studenti.unibs.it>
2022-09-08 16:43:11 +0200

    Modifica di Start

--- kernel/Start.java
+++ kernel/Start.java
@@ -7,7 +7,6 @@ public class Start
 	public static void main(String[] args)
 	{
 		String pathmps = ".\\MK\\INS_15_10_2v.dat";
-		//String pathmps = args[0];
 		String pathlog = ".";
 		String pathConfig = "config.txt";
 		Configuration config = ConfigurationReader.read(pathConfig);		
