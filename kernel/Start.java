fb675fbfaa71afc6e60662ceb3fc74eb091f6abb
Yuri <yurikume@gmail.com>
2022-09-09 15:47:23 +0200

    prova di commit senza start

--- kernel/Start.java
+++ kernel/Start.java
@@ -6,7 +6,7 @@ public class Start
 {
 	public static void main(String[] args)
 	{
-		String pathmps = ".\\MK\\INS_15_10_2v.dat";
+		//String pathmps = ".\\MK\\INS_15_10_2v.dat";
 		String pathlog = ".";
 		String pathConfig = "config.txt";
 		Configuration config = ConfigurationReader.read(pathConfig);		
