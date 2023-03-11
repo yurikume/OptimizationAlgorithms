52566c9c663695ae416c633624d7b1cc903463e4
Michele Signori <mik99.signori@gmail.com>
2023-03-11 15:36:47 +0100

    Corretto il bucket buildet by name che funzionava in modo sbagliato

--- kernel/BucketBuilderByName.java
+++ kernel/BucketBuilderByName.java
@@ -13,52 +13,42 @@ public class BucketBuilderByName implements BucketBuilder
 		
 		List<Bucket> buckets = new ArrayList<>();
 		Bucket b = new Bucket();
-		int size = (int) Math.floor(items.size()*config.getBucketSize());
+//		int size = (int) Math.floor(items.size()*config.getBucketSize());
 		
-		List<Item> y_items=items.stream().filter(p -> p.getName().startsWith("y")).collect(Collectors.toList());
-		int y_da_prendere = (int) Math.floor(y_items.size()/(items.size()/size)); 
-		// like doing y_items.size() * config.getBucketSize(), so it means i take a percentage of y variables to put in the bucket
+		// In y_items metto le y che non sono nel kernel
+		List<Item> y_items=items.stream().filter(it -> it.getName().startsWith("y") && !y_ker.contains(it)).collect(Collectors.toList());
+//		int y_da_prendere = (int) Math.floor(y_items.size()/(items.size()/size)); 
+		int y_da_prendere = (int)Math.floor(y_items.size() * config.getBucketSize());// so it means i take a percentage of y variables to put in the bucket
 		int i = 0;
+		int limit;
 		
 		while(y_items.size()>0) {
 			System.out.println("y_items = " + y_items.size());
 			
 			List<Item> x_list = new ArrayList<Item>();
 	
-			if(i < y_items.size()) {
-				for(i=0; i < y_da_prendere; i++) {			
-					Item y_item = y_items.get(i);
-					y_items.remove(i);
-					b.addItem(y_item);
-					
-					String vars[]= y_item.getName().split("_");
-		            String fam = vars[1];
-		            String knap = vars[2];
-					
-					x_list = items.stream().filter(p -> p.getName().startsWith("x_"+fam) && p.getName().endsWith("_"+knap)).collect(Collectors.toList());
-					for(Item x_item: x_list) {
-						b.addItem(x_item);
-					}
-				}
-				buckets.add(b);
-				b = new Bucket();
+			if(y_da_prendere < y_items.size()) { // Se le y da prendere sono meno degli item rimasti ancora da aggiungere
+				limit = y_da_prendere;
 			}else {
-				for(i=0; i<y_items.size(); i++) {
-					Item y_item = y_items.get(i);
-					y_items.remove(i);
-					b.addItem(y_item);
-
-					String vars[]= y_item.getName().split("_");
-		            String fam = vars[1];
-		            String knap = vars[2];
-					
-					x_list = items.stream().filter(p -> p.getName().startsWith("x_"+fam) && p.getName().endsWith("_"+knap)).collect(Collectors.toList());
-					for(Item x_item: x_list) {
-						b.addItem(x_item);
-					}
+				limit = y_items.size();
+			}
+			
+			for(i=0; i < limit; i++) {			
+				Item y_item = y_items.get(0);
+				y_items.remove(0);
+				b.addItem(y_item);
+				
+				String vars[]= y_item.getName().split("_");
+	            String fam = vars[1];
+	            String knap = vars[2];
+				
+				x_list = items.stream().filter(p -> p.getName().startsWith("x_"+fam) && p.getName().endsWith("_"+knap)).collect(Collectors.toList());
+				for(Item x_item: x_list) {
+					b.addItem(x_item);
 				}
-				buckets.add(b);
-			}		
+			}
+			buckets.add(b);
+			b = new Bucket();
 		}
 		return buckets;
 	}
